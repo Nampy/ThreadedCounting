91fc9183e1537a428aa4ee5f97d56dc324b3c468
Nampy <at_ben@encs.concordia.ca>
2018-07-09 22:37:06 -0400

    Initial commit

---
+++ src/com/company/ThreadedCounting.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.w3c.dom.css.Counter;
+
+public class ThreadedCounting {
+
+    private ThreadedCounting(){
+        //no instantiation of constructor
+    }
+    public static void countInThreads(Counter counter) {
+        // 1-100...go!
+    }
+}
